5ab71412b54a1849f4f94f6bcafe1a871d55f634
haykam821 <24855774+haykam821@users.noreply.github.com>
2023-10-12 14:58:18 -0400

    Update mod to Minecraft 1.17.1

--- src/main/java/io/github/haykam821/lightningprevention/mixin/LightningEntityMixin.java
+++ src/main/java/io/github/haykam821/lightningprevention/mixin/LightningEntityMixin.java
@@ -1,11 +1,12 @@
 package io.github.haykam821.lightningprevention.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.LightningEntity;
@@ -20,13 +21,20 @@ public class LightningEntityMixin {
 	private static final Identifier LIGHTNING_FIRE_BASE_BLOCKS_ID = new Identifier("lightningprevention", "lightning_fire_base_blocks");
 
 	@Unique
-	private static final Tag<Block> LIGHTNING_FIRE_BASE_BLOCKS = TagRegistry.block(LIGHTNING_FIRE_BASE_BLOCKS_ID);
+	private static final Tag<Block> LIGHTNING_FIRE_BASE_BLOCKS = TagFactory.BLOCK.create(LIGHTNING_FIRE_BASE_BLOCKS_ID);
+
+	@Shadow
+	private int blocksSetOnFire;
 
 	@Redirect(method = "spawnFire", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z"))
 	private boolean preventSpawningFireOnNonWhitelistedBases(World world, BlockPos pos, BlockState state) {
 		if (world.getBlockState(pos.down()).isIn(LIGHTNING_FIRE_BASE_BLOCKS)) {
 			return world.setBlockState(pos, state);
 		}
+
+		// Counteract incrementation after the World#setBlockState method call
+		this.blocksSetOnFire -= 1;
+
 		return false;
 	}
 }
--- src/main/java/io/github/haykam821/lightningprevention/mixin/ServerWorldMixin.java
+++ src/main/java/io/github/haykam821/lightningprevention/mixin/ServerWorldMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.tag.Tag;
@@ -18,7 +18,7 @@ public class ServerWorldMixin {
 	private static final Identifier PREVENT_LIGHTNING_STRIKE_BASE_BLOCKS_ID = new Identifier("lightningprevention", "prevent_lightning_strike_base_blocks");
 
 	@Unique
-	private static final Tag<Block> PREVENT_LIGHTNING_STRIKE_BASE_BLOCKS = TagRegistry.block(PREVENT_LIGHTNING_STRIKE_BASE_BLOCKS_ID);
+	private static final Tag<Block> PREVENT_LIGHTNING_STRIKE_BASE_BLOCKS = TagFactory.BLOCK.create(PREVENT_LIGHTNING_STRIKE_BASE_BLOCKS_ID);
 
 	@Redirect(method = "tickChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;hasRain(Lnet/minecraft/util/math/BlockPos;)Z"))
 	private boolean preventStrikingOnBlacklistedBases(ServerWorld world, BlockPos pos) {
